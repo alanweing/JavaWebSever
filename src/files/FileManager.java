57d5af976a6548ddc0355b4d152af218ab469233
Alan Weingartner <alan.weingartner@gmail.com>
2017-09-26 16:12:56 -0300

    Renderer removed. The process of reading files is now done in FileManager

--- src/files/FileManager.java
+++ src/files/FileManager.java
@@ -1,8 +1,9 @@
 package files;
 
+import com.sun.istack.internal.NotNull;
 import com.sun.istack.internal.Nullable;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 
 public final class FileManager {
@@ -38,5 +39,39 @@ public final class FileManager {
             }
         }
         return null;
+    }
+
+    @Nullable
+    static String[] readFile(@NotNull final File file) {
+        final ArrayList<String> lines = new ArrayList<>();
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader bReader = new BufferedReader(reader);
+            String line;
+            while ((line = bReader.readLine()) != null) {
+                lines.add(line);
+            }
+            bReader.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines.size() > 0 ? lines.toArray(new String[lines.size()]) : null;
+    }
+
+    @Nullable
+    static byte[] readFileBytes(@NotNull final File file) {
+        byte[] finalBytes = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            finalBytes = new byte[(int) file.length()];
+            bufferedInputStream.read(finalBytes, 0, finalBytes.length);
+            bufferedInputStream.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return finalBytes;
     }
 }
